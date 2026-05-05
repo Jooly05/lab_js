d8c62811e6e6a23259610f07b057e9a9aae2b379
Jooly <263285346+Jooly05@users.noreply.github.com>
2026-05-05 07:05:13 +0000

    Изменение id

--- scriptReg.js
+++ scriptReg.js
@@ -2,10 +2,10 @@ const registrationUser = document.getElementById('reg')
 registrationUser.addEventListener("click", (event) => {
     event.preventDefault()
     const name = document.getElementById('name').value
-    const second_name = document.getElementById('second_name').value
+    const second_name = document.getElementById('secname').value
     const patronymic = document.getElementById('patronymic').value
     const email = document.getElementById('email').value
-    const birthday = document.getElementById('birthday').value
+    const birthday = document.getElementById('bday').value
     const current_balance = document.getElementById('current_balance').value
     const password = document.getElementById('password').value
     console.log("Запрос: ", { name, second_name, patronymic, email, birthday, current_balance })
